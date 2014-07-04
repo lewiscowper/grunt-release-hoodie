b97afdc4c8ffcae7ee41bc7c5d1d8094108897b6
Stephan Bönnemann <stephan@boennemann.me>
2014-07-04 11:42:57 +0200

    feat(task): add debug mode for sanity

--- tasks/release_hoodie.js
+++ tasks/release_hoodie.js
@@ -29,12 +29,27 @@ module.exports = function (grunt) {
       }
     }
 
+    grunt.log.debug('Note: No comitting, tagging or pushing in debug mode');
+    grunt.log.debug(
+      'Using the following options for grunt-bump:\n' +
+      JSON.stringify(options.bump, null, 2)
+    );
+
+    if (grunt.option('debug')) {
+      options.bump.commit = options.bump.createTag = options.bump.push = false;
+    }
+
     grunt.config.set('bump', {options: options.bump});
 
     // Forward arguments to the bump-only task
     this.args.unshift('bump-only');
     options.tasks.unshift(this.args.join(':'));
     options.tasks.push('bump-commit');
+
+    grunt.log.debug(
+      'Running the following tasks: ' +
+      grunt.log.wordlist(options.tasks)
+    );
 
     grunt.task.run(options.tasks);
 
