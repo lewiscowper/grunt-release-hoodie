af923c35cfab0f1c49d787902431b35d43cd686d
Stephan Bönnemann <stephan@boennemann.me>
2014-07-21 02:01:29 +0200

    fix(integration-test): use absolute path for subgrunt location
    * * *
    
    This commit was sponsored by The Hoodie Firm.
    You can hire The Hoodie Firm:
    http://go.hood.ie/thehoodiefirm
    
    * * *
    
    This commit was sponsored by The Hoodie Firm.
    You can hire The Hoodie Firm:
    http://go.hood.ie/thehoodiefirm

--- tasks/integration-test.js
+++ tasks/integration-test.js
@@ -1,14 +1,17 @@
 module.exports = function (grunt) {
   'use strict';
 
+  var path = require('path');
+
   grunt.registerTask('integration-test', 'Run Hoodie\'s integration test suite', function() {
 
-    grunt.loadTasks(require('path').join(__dirname, '../node_modules/grunt-subgrunt/tasks'));
+    grunt.loadTasks(path.join(__dirname, '../node_modules/grunt-subgrunt/tasks'));
+
+    var integration = {};
+    integration[path.join(__dirname, '../node_modules/hoodie-integration-test')] = 'default';
 
     grunt.config.set('subgrunt', {
-      integration: {
-        'node_modules/hoodie-integration-test': 'default'
-      }
+      integration: integration
     });
 
     grunt.task.run(['subgrunt:integration']);
