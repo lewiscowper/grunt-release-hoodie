3fa2ef96f0bf420eeeaa9f2a7c257c17b687b04c
Stephan Bönnemann <stephan@boennemann.me>
2014-07-30 23:23:52 +0200

    fix(prepare-release): extract tag correctly
    * * *
    
    This commit was sponsored by The Hoodie Firm.
    You can hire The Hoodie Firm:
    http://go.hood.ie/thehoodiefirm

--- tasks/util/preparation-tag.js
+++ tasks/util/preparation-tag.js
@@ -1,14 +1,14 @@
-var semverRegex = require('semver-regex')();
+var semverRegex = require('semver-regex');
 
 var exports = module.exports = function(tag) {
   if (!exports.regex.test(tag)) {
     return false;
   }
   var version = tag.match(exports.regex)[1];
-  if (semverRegex.test(version)) {
+  if (semverRegex().test(version)) {
     return version;
   }
   return false;
 };
 
-exports.regex = /^release-v?(.+)$/;
+exports.regex = /^release-v?(.+)/;
