9b94733410c1e351875ea07f6417b62357dd06d3
Stephan Bönnemann <stephan@boennemann.me>
2014-08-01 20:39:37 +0200

    chore(grunt): it's magic :sparkles:
    * * *
    
    This commit was sponsored by The Hoodie Firm.
    You can hire The Hoodie Firm:
    http://go.hood.ie/thehoodiefirm

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,5 @@
 module.exports = function (grunt) {
 
-  grunt.initConfig({
-    release: {
-      options: {
-        bump: {
-          files: ['package.json'],
-          commitFiles: [
-            'package.json',
-            'CHANGELOG.md'
-          ]
-        }
-      },
-    }
-  });
-
   grunt.loadTasks('tasks');
 
 };
